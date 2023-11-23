e9a60b2c0369e35ef5474c250bd95eecfb7dc6b0
desamatheus <120420071+desamatheus@users.noreply.github.com>
2023-11-23 19:56:19 -0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 /** @format */
 
-console.log("ola muna d");
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+require("dotenv").config();
+const routes = require("./routes");
+
+const app = express();
+
+mongoose.connect(
+  process.env.MONGO_URI,
+  // { useUnifiedTopology: true, useNewUrlParser: true },
+  console.log("entrou no database")
+);
+app.use(cors());
+app.use(express.json());
+
+app.listen(3333, () => console.log("serv rodando"));
